92f013975b6977db80db14f5f49d57d2c96904fd
Ewerton Lopes Pereira <ewertonlopespereira@gmail.com>
2022-08-11 16:09:21 -0300

    Mesa de Trabalho 08-08 - Busca de Voos

--- src/Aula08_Facade/Mesa_De_Trabalho/FacadeTurismoTest.java
+++ src/Aula08_Facade/Mesa_De_Trabalho/FacadeTurismoTest.java
@@ -3,8 +3,22 @@ package Aula08_Facade.Mesa_De_Trabalho;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 public class FacadeTurismoTest {
+    private FacadeTurismo facadeTurismo = new FacadeTurismo();
 
+    private Viagem viagem;
 
+    @BeforeEach
+    void doBefore() {
+        viagem = new Viagem(LocalDate.of(2022, 10, 15), LocalDate.of(2022,11,15), "São Paulo", "Dubai");
+    }
+
+    @Test
+    void validarBusca() {
+        String busca = String.valueOf(facadeTurismo.buscas(viagem));
+        System.out.println(busca);
+    }
 
 }
