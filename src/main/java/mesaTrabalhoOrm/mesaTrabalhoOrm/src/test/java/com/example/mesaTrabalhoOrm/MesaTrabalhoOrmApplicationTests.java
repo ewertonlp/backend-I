2b4f0a17c97fc90f630d1eddead9a8389437e9a4
Ewerton Lopes Pereira <ewertonlopespereira@gmail.com>
2022-09-09 20:45:00 -0300

    Mesa de Trabalho 32.

---
+++ src/main/java/mesaTrabalhoOrm/mesaTrabalhoOrm/src/test/java/com/example/mesaTrabalhoOrm/MesaTrabalhoOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mesaTrabalhoOrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesaTrabalhoOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
