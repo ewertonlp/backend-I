6cbb9a3ca32deeafb380519c2197a8379940598c
Ewerton Lopes Pereira <ewertonlopespereira@gmail.com>
2022-09-09 20:52:36 -0300

    Mesa de Trabalho Aula 32

---
+++ src/main/java/filmes/filmes/src/test/java/com/aula27ApiRestII/filmes/FilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aula27ApiRestII.filmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
